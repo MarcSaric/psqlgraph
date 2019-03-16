f1a76664dea953f26eb1cca0370be6b929c8faba
Justin Barnowski <jbarno@uchicago.edu>
2019-03-16 12:13:06 -0500

    feat(fixture): fixtureizing psql database connections
    Should be made into a package for importation downstream

--- bin/setup_psqlgraph.py
+++ bin/setup_psqlgraph.py
@@ -1,9 +1,7 @@
 """
-This is a one-time use script to set up a fresh install of Postgres 9.4
 Needs to be run as the postgres user.
 """
 
-import argparse
 from sqlalchemy import create_engine
 import logging
 
@@ -12,10 +10,28 @@ from psqlgraph.node import Base
 from psqlgraph import PsqlGraphDriver
 from models import TestNode
 
+@pytest.fixture(scope="Session")
+def user():
+    return "test"
 
-def try_drop_test_data(user, database, root_user='postgres', host=''):
+@pytest.fixture(scope="Session")
+def host()
+    return "localhost"
 
-    print('Dropping old test data')
+@pytest.fixture(scope="Session")
+def password():
+    return "test"
+
+@pytest.fixture(scope="Session")
+def database():
+    return "automated_test"
+
+@pytest.fixture(scope="Session")
+def root_user():
+    return "postgres"
+
+@pytest.fixture
+def try_drop_test_data(user, database, root_user, host):
 
     engine = create_engine("postgres://{user}@{host}/postgres".format(
         user=root_user, host=host))
@@ -38,11 +54,11 @@ def try_drop_test_data(user, database, root_user='postgres', host=''):
     conn.close()
 
 
-def setup_database(user, password, database, root_user='postgres', host=''):
+@pytest.fixture
+def setup_database(user, password, database, root_user, host):
     """
     setup the user and database
     """
-    print('Setting up test database')
 
     try_drop_test_data(user, database)
 
@@ -67,36 +83,13 @@ def setup_database(user, password, database, root_user='postgres', host=''):
         logging.warn("Unable to add user:" + str(msg))
     conn.close()
 
-
 def create_tables(host, user, password, database):
     """
     create a table
     """
-    print('Creating tables in test database')
 
     driver = PsqlGraphDriver(host, user, password, database)
     Base.metadata.create_all(driver.engine)
 
-
-def create_indexes(host, user, password, database):
-    """
-    create a table
-    """
-    return
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, action="store",
-                        default='localhost', help="psql-server host")
-    parser.add_argument("--user", type=str, action="store",
-                        default='test', help="psql test user")
-    parser.add_argument("--password", type=str, action="store",
-                        default='test', help="psql test password")
-    parser.add_argument("--database", type=str, action="store",
-                        default='automated_test', help="psql test database")
-
-    args = parser.parse_args()
-    setup_database(args.user, args.password, args.database)
-    create_tables(args.host, args.user, args.password, args.database)
-    create_indexes(args.host, args.user, args.password, args.database)
+    setup_database(user, password, database)
+    create_tables(host, user, password, database)
